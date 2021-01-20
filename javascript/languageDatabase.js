5ecdf9617215206e2e90813c055320de85dac347
Ivan <ivan.ortiz1@ingka.ikea.com>
2021-01-19 22:01:54 -0500

    Finished translating the page 100%

--- javascript/languageDatabase.js
+++ javascript/languageDatabase.js
@@ -1,19 +1,164 @@
 var data = {
 	"english": {
+		//NavBar
+		"navCeremony": "Ceremony",
+		"navVenue": "Venue",
+		"navHotels": "Hotels",
+		"navTravelInformation": "Travel Information",
+		"navFAQ": "FAQs",
+
+		//RSVP Jumbotron
 		"rsvpTitle": "Marilynn & Ivan's Wedding",
 		"rsvpDate": "Friday, July 30, 2021",
+		"rsvpDeadline": "The deadline to RSVP is Sunday, March 14",
 
 		// Ceremony
 		"ceremonyTitle": "Ceremony",
-		"ceremonyNameOfChurch": "Cathedral of Morelia"
-		
+		"ceremonyNameOfChurch": "Cathedral of Morelia",
+
+		//Venue
+		"venueTitle": "Venue",
+
+		//Hotels
+		"hotelTitle": "Hotels",
+		"hotelIntroText1": "We have personally stayed at Herencia, Los Juaninos, and Hotel de La Soledad and fully recommend all three of them. The majority of hotels offer a shuttle service to and from the aiport for a fee. Ask your hotel for more information.",
+		"hotelIntroText2": "Third-party sites (e.g. Expedia, Travelocity, Hotels.com, etc) are usually cheaper than booking through the hotels directly. However, if you book through a third-party site, we advise that you contact the hotel afterwards to confirm your reservation.",
+		"hotelDateVerification": "Verify your dates when comparing on KAYAK. By default, the range is set for 5 days and 4 nights — Wednesday, 7/28 through Sunday, 8/1.",
+		"hotelPriceNight1": "~ $50/night USD",
+		"hotelPriceNight2": "~ $50/night USD",
+		"hotelPriceNight3": "~ $90/night USD",
+		"hotelPriceNight4": "~ $60/night USD",
+		"hotelPriceNight5": "~ $120/night USD",
+		"hotelPriceNight6": "~ $200/night USD",
+		"hotelWebsite1": "Website",
+		"hotelWebsite2": "Website",
+		"hotelWebsite3": "Website",
+		"hotelWebsite4": "Website",
+		"hotelWebsite5": "Website",
+		"hotelWebsite6": "Website",
+		"hotelDirections1": "Directions",
+		"hotelDirections2": "Directions",
+		"hotelDirections3": "Directions",
+		"hotelDirections4": "Directions",
+		"hotelDirections5": "Directions",
+		"hotelDirections6": "Directions",
+		"hotelCompareKAYAK1": "Compare prices on ",
+		"hotelCompareKAYAK2": "Compare prices on ",
+		"hotelCompareKAYAK3": "Compare prices on ",
+		"hotelCompareKAYAK4": "Compare prices on ",
+		"hotelCompareKAYAK5": "Compare prices on ",
+		"hotelCompareKAYAK6": "Compare prices on ",
+		"hotelDescriptionMisionCatedral": "A modest establishment with fantastic views of the cathedral and plaza. This is an affordable option that places you right in the center of everything.",
+		"hotelDescriptionAlameda": "Another great option that's also located across from the main plaza and cathedral. This hotel has a more modern feel and has great rates.",
+		"hotelDescriptionLosJuaninos": "Our number one recommendation due to its great price and location. This hotel is directly across the street from the cathedral.",
+		"hotelDescriptionHerencia": "Wonderful hotel that is a short walk away from the cathedral. Personally, this would be our second choice after Hotel Los Juaninos.",
+		"hotelDescriptionSoledad": "A crown jewel, this is one of the most luxurious accommodations in all of Morelia. Incredible hotel with fantastic architecture.",
+		"hotelDescriptionCanteraDiez": "A modern 5-star hotel with breath-taking views of the cathedral. This hotel has rates for under $200 USD on it's own site. We recommend NOT booking this hotel through a third-party site.",
+
+		//Travel Information
+		"travelInformationTitle": "Travel Information",
+		"travelInformationAirport": "General Francisco Mujica International Airport",
+		"travelInformationPassport1": "In order to travel to Mexico, you will need a valid U.S. passport. If you do not have one or if it has expired, please take the necessary steps to obtain or renew your passport. Please visit the",
+		"travelInformationPassport2": "U.S. Department of State website",
+		"travelInformationPassport3": "for more information.",
+		"travelInformationExchange1": "The currency in Mexico is the Peso and the",
+		"travelInformationExchange2": "exchange rate",
+		"travelInformationExchange3": "is usually around $19 MXN for $1 USD.",
+
+		//FAQs
+		"faqTitle": "Frequently Asked Questions",
+		"faqQuestion1": "Will I need to arrange transportation between my hotel, the cathedral, and the venue?",
+		"faqQuestion2": "Are children allowed?",
+		"faqQuestion3": "Can I bring a guest?",
+		"faqQuestion4": "Are you registered anywhere?",
+		"faqQuestion5": "What will the weather be like in Morelia during this time of year?",
+		"faqQuestion6": "I still have questions, what is the best way to contact you?",
+		"faqAnswer1": "The church, the venue, and all of the hotels listed above are all walking distance from one another! Your total commute time between the three should be 5 - 10 minutes. You will not need to arrange for transportation of any kind.",
+		"faqAnswer2": "The venue is located on a rooftop terrace and is not suitable for small children. However, we are providing a nanny service free of charge for all of our guests and it covers the duration of the entire reception. This is an accommodation provided by the venue and children ages 2 - 9 will be cared for inside of a playroom adjacent to the terrace. We will have one nanny per six kids and parents will be free to check in on their children whenever they please.",
+		"faqAnswer3": "Unfortunately, we can only admit those listed on your invitation. There is a strict limit on seating and all of the meals are accounted for according to the number of guests that RSVP'd. Thank you for your understanding.",
+		"faqAnswer4": "We ask only for your love and presence on our special day but if you were thinking of giving a gift, we would appreciate a cash gift to help us save for our new home. Thank you!",
+		"faqAnswer5": "In the summer months, Morelia is usually a perfect 70 - 80°F with clear skies and sunshine. However, it does occasionally rain so at least one rain jacket or coat is recommended.",
+		"faqAnswer6": "We know that these are uncertain times due to COVID-19. If you feel that you need an extension to RSVP or for any other questions, please send us an e-mail at ",
 	},
 	"spanish": {
+		//NavBar
+		"navCeremony": "Ceremonia",
+		"navVenue": "Recepción",
+		"navHotels": "Hoteles",
+		"navTravelInformation": "Información Para Viajar",
+		"navFAQ": "Preguntas Frecuentes",
+
 		// RSVP Jumbotron
 		"rsvpTitle": "La Boda de Marilynn y Ivan",
-		"rsvpDate": "Viernes, 30 de julio 2021"	},
+		"rsvpDate": "Viernes, 30 de julio 2021",
+		"rsvpDeadline": "Confirmar asistencia antes del domingo, 14 de marzo del 2021",
 
 		// Ceremony
 		"ceremonyTitle": "Ceremonia Religiosa",
-		"ceremonyNameOfChurch": "La Catedral De Morelia"
-};
+		"ceremonyNameOfChurch": "La Catedral De Morelia",
+
+		//Venue
+		"venueTitle": "Recepción",
+
+		//Hotels
+		"hotelTitle": "Hoteles",
+		"hotelIntroText1": "Nosotros personalmente nos hemos hospedado en los hoteles: Herencia, Los Juaninos y La Soledad y definitivamente los recomendamos. La mayoría de hoteles ofrecen transportación  hacia el hotel o al aeropuerto por una tarifa extra. Preguntar directamente a su hotel de preferencia sobre este servicio/tarifa.",
+		"hotelIntroText2": "Las terceras compañías como Expedia, Travelocity, Booking, Hotels.com etc. usualmente ofrecen tarifas más económicas que directamente reservar con el hotel. Sin embargo, les aconsejamos ponerse en contacto con el hotel para confirmar su reservación.",
+		"hotelDateVerification": "Verificar sus fechas comparando en KAYAK. Está predeterminado por 5 días, 4 noches — miércoles 27 de julio al domingo 1ro de agosto.",
+		"hotelPriceNight1": "~ $50/noche (dolares)",
+		"hotelPriceNight2": "~ $50/noche (dolares)",
+		"hotelPriceNight3": "~ $90/noche (dolares)",
+		"hotelPriceNight4": "~ $60/noche (dolares)",
+		"hotelPriceNight5": "~ $120/noche (dolares)",
+		"hotelPriceNight6": "~ $200/noche (dolares)",
+		"hotelWebsite1": "Sitio Web",
+		"hotelWebsite2": "Sitio Web",
+		"hotelWebsite3": "Sitio Web",
+		"hotelWebsite4": "Sitio Web",
+		"hotelWebsite5": "Sitio Web",
+		"hotelWebsite6": "Sitio Web",
+		"hotelDirections1": "Direcciones",
+		"hotelDirections2": "Direcciones",
+		"hotelDirections3": "Direcciones",
+		"hotelDirections4": "Direcciones",
+		"hotelDirections5": "Direcciones",
+		"hotelDirections6": "Direcciones",
+		"hotelCompareKAYAK1": "Comparar precios en ",
+		"hotelCompareKAYAK2": "Comparar precios en ",
+		"hotelCompareKAYAK3": "Comparar precios en ",
+		"hotelCompareKAYAK4": "Comparar precios en ",
+		"hotelCompareKAYAK5": "Comparar precios en ",
+		"hotelCompareKAYAK6": "Comparar precios en ",
+		"hotelDescriptionMisionCatedral": "Un establecimiento modesto con vistas fantásticas de la Catedral y Plaza de Armas. Esta es una opción económica para hospedarse ubicada en el centro de todo.",
+		"hotelDescriptionAlameda": "Otra gran opción que también está localizada en frente de la plaza principal y catedral. Este hotel es más moderno y a buen precio.",
+		"hotelDescriptionLosJuaninos": "Nuestra primera recomendación por su buen precio y ubicación. Este hotel se encuentra cruzando la calle de la catedral.",
+		"hotelDescriptionHerencia": "Un hotel maravilloso que se encuentra a una distancia muy corta, caminando, a la catedral.  Personalmente, esta sería nuestra segunda opción después de Los Juaninos.",
+		"hotelDescriptionSoledad": "¡Una hermosa joya! Este hotel es uno de los alojamientos más lujosos de Morelia por su magnífica arquitectura.",
+		"hotelDescriptionCanteraDiez": "Hotel moderno  de 5 estrellas con estupendas vistas de la catedral. Este hotel tiene tarifas de $200 dólares o menos. No recomendamos reservar por medio de otras compañías en las redes.",
+
+		//Travel Information
+		"travelInformationTitle": "Información Para Viajar",
+		"travelInformationAirport": "Aeropuerto Internacional General Francisco Mujica",
+		"travelInformationPassport1": "Para ciudadanos de Estados Unidos y poder viajar a México es necesario un Pasaporte Estadounidense. Tendrá que obtener uno y tomar las medidas necesarias para obtenerlo o renovarlo. Por favor visite el",
+		"travelInformationPassport2": "sitio web del Departamento de Estado de EE. UU.",
+		"travelInformationPassport3": "para más información.",
+		"travelInformationExchange1": "La moneda en México es el peso y el",
+		"travelInformationExchange2": "tipo de cambio",
+		"travelInformationExchange3": "es generalmente alrededor de $19 MXD por $1 USD.",
+
+		//FAQs
+		"faqTitle": "Preguntas Frecuentes",
+		"faqQuestion1": "¿Vamos a necesitar transportación entre el hotel, la catedral y la recepción?",
+		"faqQuestion2": "¿Los niños son permitidos?",
+		"faqQuestion3": "¿Podré llevar a un invitado?",
+		"faqQuestion4": "¿Están registrados en alguna parte?",
+		"faqQuestion5": "Cómo estará el clima en Morelia durante ese tiempo del año?",
+		"faqQuestion6": "Todavía tengo preguntas, ¿Cuál es la mejor manera de contactarlos?",
+		"faqAnswer1": "La iglesia, recepción y todos los hoteles mencionados se ubican a una distancia muy corta caminando! El tiempo caminando entre los tres es entre 3 a 10 minutos. No necesitas arreglar ningún tipo de transporte.",
+		"faqAnswer2": "El lugar de nuestra recepción está localizada en una  terraza no muy apta para niños pequeños. Sin embargo, estaremos ofreciendo servicio de niñera totalmente gratuito a todos nuestros invitados y cubrirá todo el evento. Esta es una acomodación proveída por nuestro lugar de recepción y niños entre 2 a 9 serán cuidados en un cuarto de juegos junto a la terraza. Tendremos una niñera para cada seis niños y los padres podrán observarlos cuando gusten.",
+		"faqAnswer3": "Desafortunadamente, solo podremos admitir a las personas (nombres) que estén en la lista de invitados. Habrá un límite de sillas correspondiente al número de invitados al igual que la cena. Esperemos que nos comprendan y gracias de antemano.",
+		"faqAnswer4": "Solo les pedimos su cariño y presencia en nuestro día especial pero si estás pensando en darnos un obsequio, te agradeceríamos un regalo monetario y ayudarnos para ahorrar en la compra de nuestro primer hogar. Muchas gracias!",
+		"faqAnswer5": "Durante el verano, el clima de Morelia es usualmente perfecto entre 70-80 F con cielos claros y soleados. Pero, ocasionalmente, llueve. Recomendamos que por lo menos lleves una chaqueta impermeable y/o paraguas.",
+		"faqAnswer6": "Sabemos que estos son tiempos inciertos debido a COVID-19. Si usted siente que necesita una extensión a RSVP o para cualquier otra pregunta, por favor envíenos un correo electrónico a ",
+	}
+}
